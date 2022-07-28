e2a4b655b01e2e6080660c52bf95374ec4cc556b
saurabh kumar <110019619+kingsaurabhkushwaha1@users.noreply.github.com>
2022-07-28 13:06:32 +0530

    my web page

---
+++ mailto.js
@@ -0,0 +1,11 @@
+const cname = document.getElementById('formname');
+const cemail = document.getElementById('formemail');
+const csubject = document.getElementById('formsubject');
+const cmessage = document.getElementById('formmessage');
+const csend = document.getElementById('send-btn');
+
+csend.addEventListener("click",()=>{
+    const link = "mailto:saurabhkushwaha0000@gmail.com?subject=" + csubject.value + "&body=" + cmessage.value + "%0A" + "%0A" + "%0A" + cname.value  + "%0A" + cemail.value;
+    console.log(link);
+    window.open(link);
+});
